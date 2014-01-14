5ca6a9e9b92ae1e1ce11a9e503405b291e4a42bb
rodrigo <rodrigo@localhost>
2014-01-14 15:56:11 -0300

    Se ha agregado al modelo del dominio Cargo, Empleado

--- App-proyecto-Web/src/main/java/com/mycompany/proyecto/model/Cargo.java
+++ App-proyecto-Web/src/main/java/com/mycompany/proyecto/model/Cargo.java
@@ -3,13 +3,15 @@ package com.mycompany.proyecto.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-//@Entity
-//@Table(name = "cargos")
+@Entity
+@Table(name = "cargos")
 public class Cargo extends NamedEntity {
 
 	private static final long serialVersionUID = 1L;
 	
 	private String descripcion;
+	
+	private int activo;
 	
 	public Cargo(){
 		
@@ -21,6 +23,14 @@ public class Cargo extends NamedEntity {
 
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
+	}
+	
+	public void setActivo(int activo) {
+		this.activo = activo;
+	}
+	
+	public int getActivo() {
+		return activo;
 	}
 	
 }
--- App-proyecto-Web/src/main/java/com/mycompany/proyecto/model/Empleado.java
+++ App-proyecto-Web/src/main/java/com/mycompany/proyecto/model/Empleado.java
@@ -1,13 +1,12 @@
 package com.mycompany.proyecto.model;
 
 import java.util.Date;
-import java.util.Set;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-//@Entity
-//@Table(name = "empleados")
+@Entity
+@Table(name = "empleados")
 public class Empleado extends Persona {
 		
 	private static final long serialVersionUID = 1L;
@@ -18,17 +17,15 @@ public class Empleado extends Persona {
 	
 	private String telefono;
 	
+	@Column(name= "fecha_nacimiento")
 	private Date fechaNacimiento;
 	
+	@Column(name = "fecha_ingreso")
 	private Date fechaIngreso;
 	
-	private Set<Ciudad> ciudad;
-	
-	private Set<Departamento> departamentos;
-	
-	private Set<Pais> paises;
-	
-	private Set<EstadoCivil> estadoCivil;
+//	private Set<Ciudad> ciudad;
+//	
+//	private Set<EstadoCivil> estadoCivil;
 	
 	private String email;
 	
@@ -78,37 +75,21 @@ public class Empleado extends Persona {
 		this.fechaIngreso = fechaIngreso;
 	}
 
-	public Set<Ciudad> getCiudad() {
-		return ciudad;
-	}
-
-	public void setCiudad(Set<Ciudad> ciudad) {
-		this.ciudad = ciudad;
-	}
-
-	public Set<Departamento> getDepartamentos() {
-		return departamentos;
-	}
-
-	public void setDepartamentos(Set<Departamento> departamentos) {
-		this.departamentos = departamentos;
-	}
-
-	public Set<Pais> getPaises() {
-		return paises;
-	}
-
-	public void setPaises(Set<Pais> paises) {
-		this.paises = paises;
-	}
-
-	public Set<EstadoCivil> getEstadoCivil() {
-		return estadoCivil;
-	}
-
-	public void setEstadoCivil(Set<EstadoCivil> estadoCivil) {
-		this.estadoCivil = estadoCivil;
-	}
+//	public Set<Ciudad> getCiudad() {
+//		return ciudad;
+//	}
+//
+//	public void setCiudad(Set<Ciudad> ciudad) {
+//		this.ciudad = ciudad;
+//	}
+//
+//	public Set<EstadoCivil> getEstadoCivil() {
+//		return estadoCivil;
+//	}
+//
+//	public void setEstadoCivil(Set<EstadoCivil> estadoCivil) {
+//		this.estadoCivil = estadoCivil;
+//	}
 
 	public String getEmail() {
 		return email;
